f5f0180931cbe0a5bd4664f642984287b8f1a348
ppeforus <64016413+ppeforus@users.noreply.github.com>
2020-04-20 18:12:26 +0545

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides() {
+  let i;
+  var slides = document.getElementsByClassName("slideshow");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+});
+
+
+
